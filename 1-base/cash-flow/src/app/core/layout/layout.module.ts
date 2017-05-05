d8f656e19c7da07903418f42bafda3924ce61527
Alberto Basalo <albertobasalo@agorabinaria.com>
2017-05-05 13:02:49 +0200

    ng g c core/layout/shell --flat --export

--- 1-base/cash-flow/src/app/core/layout/layout.module.ts
+++ 1-base/cash-flow/src/app/core/layout/layout.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
+import { ShellComponent } from './shell.component';
 
 @NgModule({
   imports: [
     CommonModule,
     LayoutRoutingModule
   ],
-  declarations: []
+  declarations: [ShellComponent],
+  exports: [ShellComponent]
 })
 export class LayoutModule { }
---
+++ 1-base/cash-flow/src/app/core/layout/shell.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-shell',
+  template: `
+    <p>
+      shell Works!
+    </p>
+  `,
+  styles: []
+})
+export class ShellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
