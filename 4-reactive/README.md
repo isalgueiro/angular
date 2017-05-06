de4b639f934edd9892675fd6a21dc826ef075094
Alberto Basalo <albertobasalo@hotmail.com>
2017-05-06 12:55:34 +0200

    shared

--- 4-reactive/README.md
+++ 4-reactive/README.md
@@ -3,5 +3,9 @@
 
  ReactiveForms
  
- 
+ ng g m core/shared
+ ng g s core/shared/forms/validators --spec false
+ ng g s core/shared/forms/tools --spec false
+ ng g c core/shared/forms/error
+ ng g c core/shared/forms/control
  ```
---
+++ 4-reactive/cash-flow/src/app/core/shared/forms/error/error.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cf-error',
+  template: `
+    <p>
+      error Works!
+    </p>
+  `,
+  styles: []
+})
+export class ErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
